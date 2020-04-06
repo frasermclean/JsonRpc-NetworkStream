2e469b953f285c498d794650d84eaaafdaad5596
Fraser McLean <dev@frasermclean.com>
2020-04-07 09:27:45 +1000

    Imported existing VS solution and projects

---
+++ Client/Client.cs
@@ -0,0 +1,72 @@
+﻿using StreamJsonRpc;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace JsonRpcClient
+{
+    class Client
+    {
+        private const int Port = 54321;
+
+        static void Main(string[] args)
+        {
+            //RequestRpcAsync().GetAwaiter().GetResult();
+            _ = RequestRpcAsync();
+            Console.WriteLine("Main() request RPC task finished. Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        static async Task RequestRpcAsync()
+        {
+            // get the local end point
+            var ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+
+            // find first ipv4 address to bind to
+            IPAddress ipAddress = null;
+            for (int i = 0; i < ipHostInfo.AddressList.Length; i++)
+            {
+                if (ipHostInfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ipHostInfo.AddressList[i];
+                    Console.WriteLine("RequestRpcAsync() Using ip address: " + ipAddress);
+                    break;
+                }
+            }
+
+            // check if we didnt find an address
+            if (ipAddress == null)
+            {
+                Console.WriteLine("Couldn't find IPV4 address.");
+                return;
+            }
+
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                await sender.ConnectAsync(ipAddress, Port);
+                Console.WriteLine("Connected to: " + sender.RemoteEndPoint);
+
+                using (var stream = new NetworkStream(sender, true))
+                {
+                    var jsonRpc = JsonRpc.Attach(stream);
+
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        int sum = await jsonRpc.InvokeAsync<int>("Add", i, i + 1);
+                        Console.WriteLine($"Sum: {i} + {i + 1} = {sum}");
+                    }
+
+                    Console.WriteLine("Terminating stream...");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occured: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,87 @@
+﻿using StreamJsonRpc;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace JsonRpcServer
+{
+    class Server
+    {
+        private static int clientId = 0;
+        private const int Port = 54321;
+
+        static void Main(string[] args)
+        {
+            //StartListeningAsync().GetAwaiter().GetResult();
+            _ = StartListeningAsync();
+            Console.ReadLine();
+        }
+
+        static async Task StartListeningAsync()
+        {
+            Console.WriteLine("StartListeningAsync() starting listening.");
+
+            // get the local end point
+            var ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            IPAddress ipAddress = null;
+
+            // find first ipv4 address to bind to
+            for (int i = 0; i < ipHostInfo.AddressList.Length; i++)
+            {
+                if (ipHostInfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ipHostInfo.AddressList[i];
+                    break;
+                }
+            }
+
+            if (ipAddress == null)
+            {
+                Console.WriteLine("Couldn't find IPV4 address.");
+                return;
+            }
+
+
+            var localEndPoint = new IPEndPoint(ipAddress, Port);
+
+            // main loop
+            while (true)
+            {
+                try
+                {
+                    using (var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                    {
+                        // bind the socket to the local endpoint and listen for incoming connections  
+                        Console.WriteLine($"Binding to: {localEndPoint.Address}");
+                        listener.Bind(localEndPoint);
+                        listener.Listen(10);
+
+                        Console.WriteLine($"Waiting for a connection on port: {Port}");
+                        Socket handler = await listener.AcceptAsync();
+                        clientId++;
+
+                        Console.WriteLine($"Accepted connection from: {handler.RemoteEndPoint}");
+
+                        using (var stream = new NetworkStream(handler, true))
+                        {
+                            var jsonRpc = JsonRpc.Attach(stream, new Server());
+                            Console.WriteLine($"Attached to client: {clientId}, waiting for JSON-RPC request.");
+                            await jsonRpc.Completion;
+                            Console.WriteLine("JSON-RPC request completed.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("StartListening() exception occurred: " + ex.Message);
+                }
+            }
+        }
+
+        public int Add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
